3969154544c1532247c1fb790dd06caf056cbecd
Parth-lemolite <parth.lemolite@gmail.com>
2024-11-29 15:34:29 +0530

    changes in ui.

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -1,14 +1,21 @@
 import React from "react";
+import MovieList from "./MovieList";
 
-const MovieCard = ({}) => {
+const MovieCard = (movie) => {
   return (
     <div className="card">
-      <img
-        src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTCXgCV-ZNb3InBCTaLdED58dF6iZJxIvCOBurktiWxXrwGc8DB"
-        alt="The Mummy"
-      />
+      <img src={movie.movieImage} alt={movie.title} />
       <div className="card-content">
-        <p className="movie-name">The Mummy</p>
+        {console.log(movie, ">>>>>>>>>>>>10")}
+        <p className="movie-name">{movie.title}</p>
+        <p className="director">Directed by: {movie.director}</p>
+        <p className="description">{movie.description}</p>
+        <p className="duration">Duration: {movie.duration}</p>
+        <p className="release-date">
+          Release Date: {new Date(movie.releaseDate).toLocaleDateString()}
+        </p>
+        <p className="rating">Rating: {movie.rating}/10</p>
+        <p className="year">Year: {movie.year}</p>
         <div className="movie-info">
           <p className="time">
             11:30 <span>14:45</span> 16:05 <span className="d3">3D</span> 18:40
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,185 +1,57 @@
 import React, { useContext } from "react";
 import Header from "../components/Header";
 import { MovieContext } from "../App";
+import moment from "moment";
 
 const Home = () => {
+  const { movies } = useContext(MovieContext);
+
   return (
     <div>
       <Header />
       <section>
         <div className="movie-card-section">
-          {/* Movie Cards */}
-          <div className="card">
-            <img
-              src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTCXgCV-ZNb3InBCTaLdED58dF6iZJxIvCOBurktiWxXrwGc8DB"
-              alt="The Mummy"
-            />
-            <div className="card-content">
-              <p className="movie-name">The Mummy</p>
-              <div className="movie-info">
-                <p className="time">
-                  11:30 <span>14:45</span> 16:05 <span className="d3">3D</span>{" "}
-                  18:40 21:00 23:15
-                </p>
+          {movies.map((i) => {
+            return (
+              <div className="card">
+                <img src={i.movieImage} />
+
+                <div className="card-content">
+                  <p className="movie-name">{i.title}</p>
+
+                  <div className="movie-info">
+                    <p className="time">
+                      {moment(i.releaseDate).format("Do MMM YYYY")}
+                      <i
+                        className="fa-solid fa-clock"
+                        style={{ marginLeft: "10px" }}
+                      ></i>
+                      <span style={{ marginLeft: "5px" }}>{i.duration}</span>{" "}
+                      <span
+                        style={{
+                          marginLeft: "10px",
+                        }}
+                      >
+                        <i
+                          className="fa-solid fa-star"
+                          style={{ color: "yellow" }}
+                        ></i>
+
+                        <span
+                          className=""
+                          style={{
+                            marginLeft: "5px",
+                          }}
+                        >
+                          {i.rating}
+                        </span>
+                      </span>{" "}
+                    </p>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-          <div className="card">
-            <img
-              src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTCXgCV-ZNb3InBCTaLdED58dF6iZJxIvCOBurktiWxXrwGc8DB"
-              alt="The Mummy"
-            />
-            <div className="card-content">
-              <p className="movie-name">The Mummy</p>
-              <div className="movie-info">
-                <p className="time">
-                  11:30 <span>14:45</span> 16:05 <span className="d3">3D</span>{" "}
-                  18:40 21:00 23:15
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="card">
-            <img
-              src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTCXgCV-ZNb3InBCTaLdED58dF6iZJxIvCOBurktiWxXrwGc8DB"
-              alt="The Mummy"
-            />
-            <div className="card-content">
-              <p className="movie-name">The Mummy</p>
-              <div className="movie-info">
-                <p className="time">
-                  11:30 <span>14:45</span> 16:05 <span className="d3">3D</span>{" "}
-                  18:40 21:00 23:15
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="card">
-            <img
-              src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTCXgCV-ZNb3InBCTaLdED58dF6iZJxIvCOBurktiWxXrwGc8DB"
-              alt="The Mummy"
-            />
-            <div className="card-content">
-              <p className="movie-name">The Mummy</p>
-              <div className="movie-info">
-                <p className="time">
-                  11:30 <span>14:45</span> 16:05 <span className="d3">3D</span>{" "}
-                  18:40 21:00 23:15
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="card">
-            <img src="https://m.media-amazon.com/images/M/MV5BMTYzODQzYjQtNTczNC00MzZhLTg1ZWYtZDUxYmQ3ZTY4NzA1XkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_.jpg" />
-
-            <div className="card-content">
-              <p className="movie-name">Wonder Woman</p>
-
-              <div className="movie-info">
-                <p className="time">
-                  11:30 <span>14:45 16:05</span> 18:40 21:00
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="card">
-            <img src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSZeZdWD3S9rSzfwlSsnqBWERtgBHR4h_6kHb_fR_6J-BObyxfK" />
-
-            <div className="card-content">
-              <p className="movie-name">Alien: Covenant</p>
-
-              <div className="movie-info">
-                <p className="time">
-                  11:30<span className="d3">3D</span>{" "}
-                  <span>
-                    14:45 16:05<span className="d3">3D</span>
-                  </span>{" "}
-                  18:40 21:00 23:15
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="card">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTX2TBaWUUMpmbhcnr0zypXQltqtQmW9wED_Y8bYrynL98MM1Wq" />
-
-            <div className="card-content">
-              <p className="movie-name">Baywatch</p>
-
-              <div className="movie-info">
-                <p className="time">
-                  <span>
-                    11:30 16:05<span className="d3">3D</span>
-                  </span>{" "}
-                  18:40 21:00 23:15
-                </p>
-              </div>
-            </div>
-          </div>
-
-          <div className="card">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXhEeDOpouHNg3A75Ngkgl-pQdWrr8ErxSuYCbb8-Tn7KcuD79" />
-
-            <div className="card-content">
-              <p className="movie-name">Pirates of the Caribbean</p>
-
-              <div className="movie-info">
-                <p className="time">
-                  11:30{" "}
-                  <span>
-                    14:45<span className="d3">3D</span> 16:05
-                    <span className="d3">3D</span>
-                  </span>{" "}
-                  18:40 21:00
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="card">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6NX1HzM5IkUhkwR1Yq7vkd9j5dqv0_Zaz5FCa2bzyJaUx9zOa" />
-
-            <div className="card-content">
-              <p className="movie-name">transformers 5</p>
-
-              <div className="movie-info">
-                <p className="time">
-                  11:30 <span>14:45 16:05</span> 18:40 21:00
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="card">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJ8wYlRSHxcAyi7TijH8FjeTLKcYsKi3qCzI8r_X0xKU8LkAn_" />
-
-            <div className="card-content">
-              <p className="movie-name">Planet of the Apes</p>
-
-              <div className="movie-info">
-                <p className="time">
-                  11:30<span className="d3">3D</span>{" "}
-                  <span>
-                    14:45 16:05<span className="d3">3D</span>
-                  </span>{" "}
-                  18:40 21:00 23:15
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="card">
-            <img src="https://www.movienewsletters.net/photos/NZL_105934R1.jpg" />
-
-            <div className="card-content">
-              <p className="movie-name">Dark Tower</p>
-
-              <div className="movie-info">
-                <p className="time">
-                  <span>
-                    11:30 16:05<span className="d3">3D</span>
-                  </span>{" "}
-                  18:40 21:00 23:15
-                </p>
-              </div>
-            </div>
-          </div>
+            );
+          })}
         </div>
         {/* ---movie-card--- */}
       </section>
